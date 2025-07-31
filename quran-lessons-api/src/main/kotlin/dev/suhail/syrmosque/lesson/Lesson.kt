4bfd5d540514bf0dfd76b1604a03c1ab8547943f
suhailfares <suhail.az.fares@gmail.com>
2025-08-01 00:56:57 +0200

    added teacher and teacher profile and user and local date to the Lesson Entity

--- quran-lessons-api/src/main/kotlin/dev/suhail/syrmosque/lesson/Lesson.kt
+++ quran-lessons-api/src/main/kotlin/dev/suhail/syrmosque/lesson/Lesson.kt
@@ -1,9 +1,14 @@
 package dev.suhail.syrmosque.lesson
 
+import dev.suhail.syrmosque.user.TeacherProfile
+import dev.suhail.syrmosque.user.User
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import java.time.LocalDate
 
 @Entity
 class Lesson (
@@ -16,4 +21,18 @@ class Lesson (
     val arabicName: String,
 
     val lessonType: LessonType,
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    val teacher: User,
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_profile_id")
+    val teacherProfile: TeacherProfile? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    val student: User,
+
+    val date : LocalDate,
 )
