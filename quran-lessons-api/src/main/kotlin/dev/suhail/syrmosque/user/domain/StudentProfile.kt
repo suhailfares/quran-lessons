da416e781ae82a005925f0cfdded47e99dac66e7
suhailfares <suhail.az.fares@gmail.com>
2025-09-04 00:57:08 +0200

    added quran progress to the student profile model

--- quran-lessons-api/src/main/kotlin/dev/suhail/syrmosque/user/domain/StudentProfile.kt
+++ quran-lessons-api/src/main/kotlin/dev/suhail/syrmosque/user/domain/StudentProfile.kt
@@ -1,7 +1,10 @@
 package dev.suhail.syrmosque.user.domain
 
+import dev.suhail.syrmosque.quran.domain.SurahProgress
+
 data class StudentProfile(
     val id: Long = 0,
     val userId: Long,
     val teacherId: Long? = null,
+    var quranProgress: List<SurahProgress> = emptyList(),
 )
