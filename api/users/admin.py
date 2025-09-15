e8e60d1f093f31cac63db4481a2d2bf617d72ec4
suhailfares <suhail.az.fares@gmail.com>
2025-09-15 15:19:05 +0200

    added admin for user

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
 
 # Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("phone_number", "mosque_name", "role")}),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "role")
