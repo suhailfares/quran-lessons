06bec53e3807cd3d70c538f6df7595d6c2cb11c9
suhailfares <suhail.az.fares@gmail.com>
2025-09-16 02:48:21 +0200

    IsTeacher class and function to check if user is teacher. StudentViewSet for crud and 2 functions for using the right serializer

--- api/students/views.py
+++ api/students/views.py
@@ -4,8 +4,25 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from .models import Student
-from .serializers import StudentSerializer
+from rest_framework import permissions
+from .serializers import StudentReadSerializer, StudentWriteSerializer
+
+class IsTeacher(permissions.BasePermission):
+    """
+       Custom permission to allow only teachers to create/manage students.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == "teacher"
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
-    serializer_class = StudentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsTeacher]
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return StudentReadSerializer
+        return StudentWriteSerializer
+
+
+    def perform_create(self, serializer):
+        serializer.save(teacher=self.request.user)
