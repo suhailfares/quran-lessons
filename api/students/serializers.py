2d3916d11fbc6e45aef0f4d257bdc233f1e585b3
suhailfares <suhail.az.fares@gmail.com>
2025-09-16 02:47:43 +0200

    Student write and read serializers for getting and creating students

--- api/students/serializers.py
+++ api/students/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 from .models import Student
-from users.models import User
 
-class StudentSerializer(serializers.ModelSerializer):
-    teacher_id = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.filter(role="teacher"),
-        source="teacher",
-        write_only=True
-    )
-    teacher = serializers.StringRelatedField(read_only=True)
+
+class StudentWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = [
+            "first_name", "last_name", "father_name", "mother_name",
+            "date_of_birth", "phone_number", "parent_phone_number",
+            "birth_place", "school"
+        ]
+
+
+class StudentReadSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Student
@@ -16,6 +20,5 @@ class StudentSerializer(serializers.ModelSerializer):
             "id", "first_name", "last_name", "father_name", "mother_name",
             "date_of_birth", "phone_number", "parent_phone_number",
             "birth_place", "school", "created_at",
-            "teacher", "teacher_id"
+            "teacher_id"
         ]
-
