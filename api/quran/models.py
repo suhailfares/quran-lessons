c345261edd0b9a1f526a6fb37f3b18b5ca83f8f7
suhailfares <suhail.az.fares@gmail.com>
2025-10-21 00:13:04 +0200

    added the relation between a verse and a part (juz)

--- api/quran/models.py
+++ api/quran/models.py
@@ -6,7 +6,7 @@ class Part(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     arTitle = models.CharField(max_length=100, blank=True)
-    index = models.PositiveIntegerField()
+    index = models.PositiveIntegerField(unique=True)
 
     def __str__(self):
         return self.title
@@ -15,7 +15,7 @@ class Chapter(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     arTitle = models.CharField(max_length=100, blank=True)
-    index = models.PositiveIntegerField()
+    index = models.PositiveIntegerField(unique=True)
     parts = models.ManyToManyField(
         Part,
         related_name="chapters",
@@ -30,6 +30,13 @@ class Verse(models.Model):
         on_delete=models.PROTECT,
         related_name="verses",
     )
+
+    part = models.ForeignKey(
+        Part,
+        on_delete=models.PROTECT,
+        related_name="verses",
+    )
+
     index = models.PositiveIntegerField()
     text = models.TextField(blank=True)
 
