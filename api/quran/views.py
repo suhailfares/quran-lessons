4633eb53bb9899d329d6228cca5391c2e964b1c0
suhailfares <suhail.az.fares@gmail.com>
2025-10-22 20:55:11 +0200

    added the view for adding a hifz entry

--- api/quran/views.py
+++ api/quran/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# quran/views.py
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from quran.serializers import StudentHifzCreateSerializer
+from quran.permissions import IsTeacher
+
+class StudentHifzCreateView(APIView):
+    permission_classes = [IsAuthenticated, IsTeacher]
+
+    def post(self, request, *args, **kwargs):
+        ser = StudentHifzCreateSerializer(data=request.data, context={"request": request})
+        ser.is_valid(raise_exception=True)
+        obj = ser.save()
+
+        return Response({
+            "id": obj.id,
+            "student_id": obj.student_id,
+            "chapter_index": obj.chapter.index,
+            "start": obj.start_verse,
+            "end": obj.end_verse,
+            "notes": obj.notes,
+            "created_at": obj.created_at,
+        }, status=status.HTTP_201_CREATED)
