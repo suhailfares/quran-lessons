a66fc95960b05b1bc53ca750fe64f8a6a4a03f17
suhailfares <suhail.az.fares@gmail.com>
2025-10-22 20:58:05 +0200

    added the admin code for viewing the student hifz on the admin panel

--- api/students/admin.py
+++ api/students/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from students.models import Student
+
+
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "father_name",
+        "mother_name",
+        "school",
+        "teacher",
+        "created_at",
+    )
+    list_filter = ("school", "teacher", "created_at")
+    search_fields = (
+        "first_name",
+        "last_name",
+        "father_name",
+        "mother_name",
+        "school",
+        "teacher__username",
+    )
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at",)
