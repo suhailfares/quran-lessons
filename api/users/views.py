83bab5ad7cba6c24c54cc3103c07b0517af418f2
suhailfares <suhail.az.fares@gmail.com>
2025-11-12 23:29:33 +0100

    removed unnecessary endpoints and added schema tags for Swagger

--- api/users/views.py
+++ api/users/views.py
@@ -1,19 +1,11 @@
-from rest_framework import viewsets
-from .models import User
-from .serializers import UserCreateSerializer, UserReadSerializer
-from rest_framework import permissions
+from drf_spectacular.utils import extend_schema
+from rest_framework import generics, permissions
 
+from users.models import User
+from users.serializers import UserCreateSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
+@extend_schema(tags=["Users"])
+class UserCreateView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_serializer_class(self):
-        if self.action in ["create", "update", "partial_update"]:
-            return UserCreateSerializer
-        return UserReadSerializer
-
-    def get_permissions(self):
-        if self.action == "create":
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated]
+    serializer_class = UserCreateSerializer
+    permission_classes = [permissions.AllowAny]
