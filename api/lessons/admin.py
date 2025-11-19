2a30aacd5ff9785d542a12b515e133508b98ab19
suhailfares <suhail.az.fares@gmail.com>
2025-11-19 23:58:39 +0100

    registered attendance and lesson in admin

--- api/lessons/admin.py
+++ api/lessons/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Lesson, Attendance
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("id", "subject", "teacher")
+    ordering = ("id",)
+    search_fields = ("subject", "teacher__first_name", "teacher__last_name", "teacher__email")
+
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ("id", "student", "lesson", "attended")
+    ordering = ("lesson", "student")
+    list_filter = ("lesson", "attended")
+    search_fields = ("student__first_name", "student__last_name", "lesson__subject")
