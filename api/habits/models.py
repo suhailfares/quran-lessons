e7368868e3e6f1bc7ab949aa0485dd4c48dac7e1
suhailfares <suhail.az.fares@gmail.com>
2026-01-11 19:22:43 +0100

    added StudentPoints model

--- api/habits/models.py
+++ api/habits/models.py
@@ -17,3 +17,26 @@ class Habit(models.Model):
 
     def __str__(self):
         return self.name
+
+class StudentPoints(models.Model):
+    student = models.ForeignKey(
+        Student,
+        on_delete=models.CASCADE,
+        related_name="point_records"
+    )
+
+    habit = models.ForeignKey(
+        Habit,
+        on_delete=models.CASCADE,
+    )
+
+    teacher = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+    points = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "self.student.name" + " self.habit.name"
