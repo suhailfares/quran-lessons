fc2bdea8f1f837005d64943e5177d8c333058ff2
suhailfares <suhail.az.fares@gmail.com>
2026-01-11 19:23:40 +0100

    added student points view set

--- api/habits/views.py
+++ api/habits/views.py
@@ -24,3 +24,40 @@ class HabitViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(teacher=self.request.user)
+
+
+class StudentPointsViewSet(ModelViewSet):
+    serializer_class = StudentPointsSerializer
+    permission_classes = [IsAuthenticated, IsTeacher]
+
+    def get_queryset(self):
+        """
+        Teachers can only see point records they created.
+        """
+        return StudentPoints.objects.filter(
+            teacher=self.request.user
+        ).select_related("student", "habit")
+
+    def perform_create(self, serializer):
+        """
+        Automatically assign the logged-in teacher.
+        """
+        serializer.save(teacher=self.request.user)
+
+    def perform_update(self, serializer):
+        """
+        Prevent changing ownership.
+        """
+        if serializer.instance.teacher != self.request.user:
+            raise PermissionDenied("You cannot modify this record.")
+
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        """
+        Prevent deleting others' records.
+        """
+        if instance.teacher != self.request.user:
+            raise PermissionDenied("You cannot delete this record.")
+
+        instance.delete()
