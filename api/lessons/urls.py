d87a8b9324a22f7846ad0bb4c6ea2b8a01dffcf3
suhailfares <suhail.az.fares@gmail.com>
2026-03-26 00:32:11 +0100

    extra endpoint for getting all teacher attendances

--- api/lessons/urls.py
+++ api/lessons/urls.py
@@ -4,5 +4,6 @@ from lessons.views import LessonView, AttendanceView
 
 urlpatterns = [
     path("lessons/", LessonView.as_view(), name="lessons"),
-    path("lessons/<int:lesson_id>/attendances", AttendanceView.as_view(), name="attendances")
+    path("lessons/attendances", AttendanceView.as_view(), name="attendances"),
+    path("lessons/<int:lesson_id>/attendances", AttendanceView.as_view(), name="lesson-attendances"),
 ]
