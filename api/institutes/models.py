1dd54ac372c77142212c624dceda66814179232a
suhailfares <suhail.az.fares@gmail.com>
2026-05-02 15:42:33 +0200

    new model Institute

--- api/institutes/models.py
+++ api/institutes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Institute(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
