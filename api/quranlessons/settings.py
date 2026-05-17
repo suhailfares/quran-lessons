f6f6e56a23d997fe38910f6aec22b6275410f8c1
suhailfares <suhail.az.fares@gmail.com>
2026-05-17 19:41:08 +0200

    Configure static files for Railway

--- api/quranlessons/settings.py
+++ api/quranlessons/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'lessons', # Lessons, Attendances
     'habits',
     "drf_spectacular",
-    "drf_spectacular_sidecar",
+    #"drf_spectacular_sidecar",
 ]
 
 MIDDLEWARE = [
@@ -146,9 +146,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
