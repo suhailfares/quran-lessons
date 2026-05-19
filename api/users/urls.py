a2fce6e5e27d6a028a76fb2b6ac3bcc6eec7035b
Jamil Faris <95693985+JamilmFaris@users.noreply.github.com>
2026-05-19 16:56:19 +0300

    Add users delete and listing
    Update urls.py

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from users.views import ChangePasswordView, CurrentUserView, UserCreateView
+from users.views import ChangePasswordView, CurrentUserView, UserCreateView, UserDeleteView, UserListView
 
 urlpatterns = [
     path("users/", UserCreateView.as_view(), name="user-create"),
+    path("users/all/", UserListView.as_view(), name="user-list"),
     path("users/me/", CurrentUserView.as_view(), name="user-me"),
     path("users/me/change-password/", ChangePasswordView.as_view(), name="user-change-password"),
+    path("users/<str:username>/", UserDeleteView.as_view(), name="user-delete"),
 ]
