e3ff2d4c1275516ec4379848cf9615293fcbed63
Jamil Faris <jamilfaris2000@gmail.com>
2026-07-01 07:26:52 +0300

    Add admin creation guide in the run.ps1

--- api/run.ps1
+++ api/run.ps1
@@ -4,3 +4,11 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 python manage.py runserver 0.0.0.0:8000
 python manage.py seed_data
 python manage.py seed_quran
+
+
+# Create Admin
+setx ADMIN_PASSWORD "yourpassword"
+# then restart terminal then
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+.\env\Scripts\Activate.ps1
+python manage.py create_admin
